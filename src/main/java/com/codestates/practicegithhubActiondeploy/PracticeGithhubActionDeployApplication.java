d5aacacedfc0ca2bd34747c836ec3e09a4486959
song4529 <gusdn4529@gmail.com>
2023-06-01 13:17:25 +0900

    first commit

---
+++ src/main/java/com/codestates/practicegithhubActiondeploy/PracticeGithhubActionDeployApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.practicegithhubActiondeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeGithhubActionDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeGithhubActionDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/practicegithhubActiondeploy/PracticeGithhubActionDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.practicegithhubActiondeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGithhubActionDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
